e1b54cc3cc74421292b783fcc7297f66eba28b1d
cortex05 <havinginitiative@gmail.com>
2025-04-22 15:19:19 -0500

    feat: Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# Initial attempt to recreate and then improve role playing project
+
+# Import enemy and hero data
+#import necessary functions in java
+
+# MAIN FUNCTION START
+
+# Make player variable from HERO type, set type to null
+# player changed to result of initial_config
+
+  # INITIAL CONFIG METHOD (Accept player as value)
+  
+  # scanner?
+  # boolean finished = false
+  # ask for name, input
+  # 
+
