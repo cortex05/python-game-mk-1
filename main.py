beefcfa67da8d1f6ffe545b115cf6ee008ec6243
cortex05 <havinginitiative@gmail.com>
2025-04-22 15:44:06 -0500

    feat: Intro loop and Intro scroll

--- main.py
+++ main.py
@@ -7,12 +7,23 @@
 
 # Make player variable from HERO type, set type to null
 # player changed to result of initial_config
-
   # INITIAL CONFIG METHOD (Accept player as value)
-  
-  # scanner?
-  # boolean finished = false
-  # ask for name, input
-  # MORES
 
-print("Hello!!!!")
+    # scanner?
+    # boolean finished = false
+    # ask for name, input
+    # MAKE LOOP
+      # Say Hello
+      # ask for class, offer options for characters or quit
+      # for each selection, explain their skills, ask for confirmation, (if not repeat), 
+      # if yes, set finished to true
+  # END INITIAL CONFIG
+
+# Do intro scroll 
+  # INTRO SCROLL METHOD
+    # initialize name
+    # give the spiel
+  # END SWAMP LOOP
+
+# start sawmp loop
+  # STARRT LOOP METHOD
