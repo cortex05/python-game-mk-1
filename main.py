5faa4c751c3af55331a1154773cb8b6bdd17c2cc
cortex05 <havinginitiative@gmail.com>
2025-07-03 14:49:12 -0500

    tweak initialization with error handling

--- main.py
+++ main.py
@@ -31,49 +31,51 @@ print(f"\nHello {player_name}")
 def initialiationLoop(name):
   player = ''
   while True:
-    character_choice = input('''
-    Please Select your class
-    1) Elf
-    2) Dwarf
-    3) Swordsman
-    4) Quit
-    \n''')
+    try: 
+      character_choice = int(input('''
+        Please Select your class
+        1) Elf
+        2) Dwarf
+        3) Swordsman
+        4) Quit
+      \n'''))
 
-  # selection check
-    if character_choice == '1':
-      proto_selection = descriptions[int(character_choice) - 1]
-      selection = input(f'''\nYou choose {proto_selection["name"]}. \n \n{proto_selection["name"]} is {proto_selection["description"]}. \n \nDo you wish this to be your character? Y/N \n''')
-      if selection.lower() == 'y':
-        player = characters.Elf.Elf(name)
+      if character_choice == 1:
+        proto_selection = descriptions[character_choice - 1]
+        selection = input(f'''\nYou choose {proto_selection["name"]}. \n \n{proto_selection["name"]} is {proto_selection["description"]}. \n \nDo you wish this to be your character? Y/N \n''')
+        if selection.lower() == 'y':
+          player = characters.Elf.Elf(name)
+          break
+        else:
+          continue
+      elif  character_choice == 2:
+        proto_selection = descriptions[int(character_choice) - 1]
+        selection = input(f'''\nYou choose {proto_selection["name"]}. \n \n{proto_selection["name"]} is {proto_selection["description"]}. \n \nDo you wish this to be your character? Y/N \n''')
+        if selection.lower() == 'y':
+          player = characters.Dwarf.Dwarf(name)
+          break
+        else:
+          continue
+      elif  character_choice == 3:
+        proto_selection = descriptions[int(character_choice) - 1]
+        selection = input(f'''\nYou choose {proto_selection["name"]}. \n \n{proto_selection["name"]} is {proto_selection["description"]}. \n \nDo you wish this to be your character? Y/N \n''')
+        if selection.lower() == 'y':
+          player = characters.Swordsman.Swordsman(name)
+          break
+        else:
+          continue
+      elif character_choice == 4:
+        print("Goodbye")
         break
       else:
-        continue
-    elif  character_choice == '2':
-      proto_selection = descriptions[int(character_choice) - 1]
-      selection = input(f'''\nYou choose {proto_selection["name"]}. \n \n{proto_selection["name"]} is {proto_selection["description"]}. \n \nDo you wish this to be your character? Y/N \n''')
-      if selection.lower() == 'y':
-        player = characters.Dwarf.Dwarf(name)
-        break
-      else:
-        continue
-    elif  character_choice == '3':
-      proto_selection = descriptions[int(character_choice) - 1]
-      selection = input(f'''\nYou choose {proto_selection["name"]}. \n \n{proto_selection["name"]} is {proto_selection["description"]}. \n \nDo you wish this to be your character? Y/N \n''')
-      if selection.lower() == 'y':
-        player = characters.Swordsman.Swordsman(name)
-        break
-      else:
-        continue
-    elif character_choice == '4':
-      print("Goodbye")
-      break
-    else:
-      print("Please enter a valid number\n")
-      continue  
+        print("Please enter a whole number between 0 and 5\n")
+        continue  
 
+    except ValueError:
+      print("Please enter a number")
+    continue
+      
   return player
-  # if yes, set finished to true
-  # END INITIAL CONFIG
 
 
 player = initialiationLoop(player_name)
